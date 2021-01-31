af0e61befe3e9d372837c008aa651989c4b0a084
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2021-01-31 14:32:57 -0600

    Fixed mixin

---
+++ src/main/java/net/ludocrypt/musicdr/config/MusicDrConfig.java
@@ -0,0 +1,26 @@
+package net.ludocrypt.musicdr.config;
+
+import java.io.File;
+
+import com.electronwill.nightconfig.core.file.CommentedFileConfig;
+import com.electronwill.nightconfig.core.io.WritingMode;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class MusicDrConfig {
+	private static final ForgeConfigSpec.Builder client_builder = new ForgeConfigSpec.Builder();
+	public static final ForgeConfigSpec client_config;
+
+	public static void loadConfig(ForgeConfigSpec config, String path) {
+		final CommentedFileConfig file = CommentedFileConfig.builder(new File(path)).sync().autosave().writingMode(WritingMode.REPLACE).build();
+		file.load();
+		config.setConfig(file);
+	}
+
+	static {
+		MusicConfig.init(client_builder);
+		client_config = client_builder.build();
+	}
+}
---
+++ src/main/java/net/ludocrypt/musicdr/mixin/MusicMixin.java
@@ -0,0 +1,39 @@
+package net.ludocrypt.musicdr.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.ludocrypt.musicdr.config.MusicConfig;
+import net.minecraft.client.audio.BackgroundMusicSelector;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+@Mixin(BackgroundMusicSelector.class)
+public class MusicMixin {
+
+	@Inject(method = "getMinDelay", at = @At("RETURN"), cancellable = true)
+	private void getMinDelay(CallbackInfoReturnable<Integer> ci) {
+
+		if (MusicConfig.divide.get()) {
+			ci.setReturnValue(ci.getReturnValue() / MusicConfig.division.get());
+		} else {
+			ci.setReturnValue(Math.min(ci.getReturnValue(), Math.min(MusicConfig.minDelay.get() * 20, MusicConfig.maxDelay.get() * 20)));
+		}
+
+	}
+
+	@Inject(method = "getMaxDelay", at = @At("RETURN"), cancellable = true)
+	private void getMaxDelay(CallbackInfoReturnable<Integer> ci) {
+
+		if (MusicConfig.divide.get()) {
+			ci.setReturnValue(ci.getReturnValue() / MusicConfig.division.get());
+		} else {
+			ci.setReturnValue(Math.min(ci.getReturnValue(), Math.max(MusicConfig.minDelay.get() * 20, MusicConfig.maxDelay.get() * 20)));
+		}
+
+	}
+
+}
