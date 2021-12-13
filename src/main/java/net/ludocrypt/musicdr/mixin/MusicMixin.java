450b296355882a0c73a1e360caca1d0989b13334
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2021-12-13 15:02:36 -0600

    Update 1.18

--- src/main/java/net/ludocrypt/musicdr/mixin/MusicMixin.java
+++ src/main/java/net/ludocrypt/musicdr/mixin/MusicMixin.java
@@ -6,34 +6,30 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.ludocrypt.musicdr.config.MusicConfig;
-import net.minecraft.client.audio.BackgroundMusicSelector;
+import net.minecraft.sounds.Music;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-@Mixin(BackgroundMusicSelector.class)
+@Mixin(Music.class)
 public class MusicMixin {
 
 	@Inject(method = "getMinDelay", at = @At("RETURN"), cancellable = true)
-	private void musicDr_getMinDelay(CallbackInfoReturnable<Integer> ci) {
-
+	private void musicDr$getMinDelay(CallbackInfoReturnable<Integer> ci) {
 		if (MusicConfig.divide.get()) {
 			ci.setReturnValue(ci.getReturnValue() / MusicConfig.division.get());
 		} else {
 			ci.setReturnValue(Math.min(ci.getReturnValue(), Math.min(MusicConfig.minDelay.get() * 20, MusicConfig.maxDelay.get() * 20)));
 		}
-
 	}
 
 	@Inject(method = "getMaxDelay", at = @At("RETURN"), cancellable = true)
-	private void musicDr_getMaxDelay(CallbackInfoReturnable<Integer> ci) {
-
+	private void musicDr$getMaxDelay(CallbackInfoReturnable<Integer> ci) {
 		if (MusicConfig.divide.get()) {
 			ci.setReturnValue(ci.getReturnValue() / MusicConfig.division.get());
 		} else {
 			ci.setReturnValue(Math.min(ci.getReturnValue(), Math.max(MusicConfig.minDelay.get() * 20, MusicConfig.maxDelay.get() * 20)));
 		}
-
 	}
 
 }
--- src/main/java/net/ludocrypt/musicdr/mixin/SimpleSoundInstanceMixin.java
+++ src/main/java/net/ludocrypt/musicdr/mixin/SimpleSoundInstanceMixin.java
@@ -8,24 +8,24 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.ludocrypt.musicdr.config.ExperimentalMusicConfig;
-import net.minecraft.client.audio.ISound;
-import net.minecraft.client.audio.SimpleSound;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvent;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(value = Dist.CLIENT)
-@Mixin(value = SimpleSound.class, priority = 100)
-public class SimpleSoundMixin {
+@Mixin(value = SimpleSoundInstance.class, priority = 100)
+public class SimpleSoundInstanceMixin {
 
-	@Inject(method = "music(Lnet/minecraft/util/SoundEvent;)Lnet/minecraft/client/audio/SimpleSound;", at = @At("RETURN"), cancellable = true)
-	private static void musicDr_changePitch(SoundEvent music, CallbackInfoReturnable<SimpleSound> ci) {
+	@Inject(method = "forMusic(Lnet/minecraft/sounds/SoundEvent;)Lnet/minecraft/client/resources/sounds/SimpleSoundInstance;", at = @At("RETURN"), cancellable = true)
+	private static void musicDr$changePitch(SoundEvent music, CallbackInfoReturnable<SimpleSoundInstance> ci) {
 		Random random = new Random();
 		if (ExperimentalMusicConfig.distortPitch.get() && (random.nextDouble() < ExperimentalMusicConfig.chanceToPitchChange.get())) {
 			int note = random.nextInt((ExperimentalMusicConfig.maxNoteChange.get() - ExperimentalMusicConfig.minNoteChange.get()) + 1) + ExperimentalMusicConfig.minNoteChange.get();
 			double newPitch = Math.pow(2.0D, (double) (note) / 12.0D);
-			ci.setReturnValue(new SimpleSound(music.getName(), SoundCategory.MUSIC, 1.0F, (float) newPitch, false, 0, ISound.AttenuationType.NONE, 0.0D, 0.0D, 0.0D, true));
+			ci.setReturnValue(new SimpleSoundInstance(music.getLocation(), SoundSource.MUSIC, 1.0F, (float) newPitch, false, 0, SoundInstance.Attenuation.NONE, 0.0D, 0.0D, 0.0D, true));
 		}
 	}
 
